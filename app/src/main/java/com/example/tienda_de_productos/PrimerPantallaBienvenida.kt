9a81a9791f4d5f22dc4663d5a915b610ded4fe06
gerson <gersoncornelio1@gmail.com>
2022-09-08 11:06:27 -0500

    PrimerCommint

---
+++ app/src/main/java/com/example/tienda_de_productos/PrimerPantallaBienvenida.kt
@@ -0,0 +1,11 @@
+package com.example.tienda_de_productos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrimerPantallaBienvenida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_primer_pantalla_bienvenida)
+    }
+}
